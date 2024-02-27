2f50e8b1e9fb05d3bb91bf9c026b5d84b7a80712
Subash7Lingden <subashsubbalingden@gmail.com>
2024-02-27 23:25:52 +0545

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys 
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -16,6 +17,6 @@ class CustomException(Exception): ## custamize class inheriting exception
 
     def __str__(self): ## raising the custom exception will print error message
         return self.error_meaasge
-    
- 
+
+
     
